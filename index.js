bc6e1851d2592275ea19077a7850cdc0763f06a0
Avinash Kumar <avinash.kumar70008@gmail.com>
2022-02-02 22:32:51 -0800

    day1

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+let a=20;
+let b =10;
+console.log(add(a,b));
+console.log(subtract(a,b));
+console.log(multiply(a,b));
+console.log(divide(a,b));
